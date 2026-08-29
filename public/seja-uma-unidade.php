9fcdb1d43dee1520eca247c48712aff262030e5e
Assistente <avaset.net@gmail.com>
2026-08-29 01:04:10 -0300

    feat(site): adicionar programa de parceiros

---
+++ public/seja-uma-unidade.php
@@ -0,0 +1,7 @@
+<?php
+$tipo = 'unidade';
+$marca = 'ALFAPLENO';
+$logoNegativa = 'assets/img/alfapleno-negativo.png';
+$logoNormal = 'assets/img/alfapleno.png';
+$corTema = '#0c3f9e';
+require __DIR__ . '/_parceria.php';
